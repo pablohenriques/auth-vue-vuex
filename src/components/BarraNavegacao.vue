6d25a87d55ed9fb914e0f550e1e7d8e406647aff
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-03-05 16:01:03 -0300

    Autenticacao de usuario

--- src/components/BarraNavegacao.vue
+++ src/components/BarraNavegacao.vue
@@ -6,41 +6,24 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="navbar-collapse collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto" v-if="usuarioEstaLogado">
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/">Home</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/gerentes" class="nav-link">
-                            Gerentes
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" @click.prevent="efetuarLogout" class="nav-link">
-                            Logout
-                        </a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav mr-auto" v-else>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/login">Login</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/cadastre-se" class="nav-link">Registre-se</router-link>
-                    </li>
-                </ul>
+                <BarraNavegacaoQuandoLogado v-if="usuarioEstaLogado"/>
+                <BarraNavegacaoQuandoDeslogado v-else/>
             </div>
         </nav>
     </div>
 </template>
 
 <script>
+
+import BarraNavegacaoQuandoLogado from './BarraNavegacaoQuandoLogado';
+import BarraNavegacaoQuandoDeslogado from './BarraNavegacaoQuandoDeslogado';
+
+
 export default {
-    methods: {
-        efetuarLogout() {
-            localStorage.removeItem('token');
-            this.$router.push({ name: 'login' });
-        }
+
+    components: {
+        BarraNavegacaoQuandoLogado,
+        BarraNavegacaoQuandoDeslogado
     },
 
     computed: {
